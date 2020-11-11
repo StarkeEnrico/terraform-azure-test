dc2af31e6bf8fbcfca10b9221661bd78c8686a2b
Enrico Starke <enrico.starke@outlook.de>
2020-11-11 11:01:45 +0100

    nlxapp0010

--- terraform-azure-test-app-services.tf
+++ terraform-azure-test-app-services.tf
@@ -24,3 +24,29 @@ module "nlxapp0009" {
   created_by               = "%CreatedBy%"
   cost_center              = "%CostCenter%"
 }
+module "nlxapp0010" {
+  source                  = "bitcloud-terraform.softwareone.com/BITCloud/module-app-service/azure"
+  version                 = "0.0.2"
+  location                = "%Location%"
+  id                      = "%Id%""
+  tier                    = "%Tier%"
+  size                    = "%Size%"
+  always_on               = %AlwaysOn%
+  use_32                  = %Use32%
+  dotnet_version          = "%DotnetVersion%"
+  ip_restrictions = [
+    {
+      name = "IP"
+      cidr_ip = "10.0.1.2/32"
+    },
+    {
+      name = "SUBNET"
+      cidr_ip = "10.0.1.0/24"
+    }
+  ]
+  service_level_class      = "%ServiceLevelClass%"
+  owned_by                 = "%OwnedBy%"
+  managed_by               = "%ManagedBy"
+  created_by               = "%CreatedBy%"
+  cost_center              = "%CostCenter%"
+}
